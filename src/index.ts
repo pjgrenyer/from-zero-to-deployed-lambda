0c94207e7b7b058c78c36cef651cf393446d7fae
Paul Grenyer <paul.grenyer@gmail.com>
2025-10-05 12:37:22 +0100

    feat: added string reversing.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 export const handler = async (event: { text: string }) => {
     console.log(event);
     const response = {
-        statusCode: 200,
-        body: JSON.stringify(event, null, 2),
+        body: {
+            text: event.text,
+            output: event.text.split("").reverse().join("")
+        },
     };
     return response;
 };
