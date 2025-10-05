833034d0ba1e5c8a26def94b0a0a384c2c8dba7b
Paul Grenyer <paul.grenyer@gmail.com>
2025-10-05 12:48:47 +0100

    feat: added handler test.

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -1,13 +1,13 @@
-import { handler } from "../src/index";
+import { handler } from '../src/index';
 
-describe("Handler Test", () => {
-    it.skip("should return input text and reversed text", async () => {
-        const event = { text: "Hello, World!" };
+describe('Handler Test', () => {
+    it.skip('should return input text and reversed text', async () => {
+        const event = { text: 'Hello, World!' };
         expect(await handler(event)).toEqual({
             body: {
-                text: "Hello, World!",
-                output: "!dlroW ,olleH",
+                text: 'Hello, World!',
+                output: '!dlroW ,olleH',
             },
         });
     });
-})
+});
