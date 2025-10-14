735546e3ff373963b3d809a46d786f694e1474d6
Paul Grenyer <paul.grenyer@haven.com>
2025-10-14 11:18:00 +0100

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import { reverse } from "./reverse";
+
 export const handler = async (event: { text: string }) => {
     console.log(event);
     const response = {
-        statusCode: 200,
-        body: JSON.stringify(event),
+        body: {
+            text: event.text,
+            output: reverse(event.text)
+        },
     };
     return response;
 };
